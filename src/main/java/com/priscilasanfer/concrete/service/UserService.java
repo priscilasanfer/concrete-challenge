35e178cb9c9d02fc0d2a84506d23f20788398d92
Priscila Ferreira <priscilasanfer@gmail.com>
2021-02-09 13:47:57 -0300

    Add service layer

---
+++ src/main/java/com/priscilasanfer/concrete/service/UserService.java
@@ -0,0 +1,72 @@
+package com.priscilasanfer.concrete.service;
+
+import com.priscilasanfer.concrete.dto.request.UserRequest;
+import com.priscilasanfer.concrete.dto.request.UserUpdateRequest;
+import com.priscilasanfer.concrete.dto.response.UserResponse;
+import com.priscilasanfer.concrete.exception.EmailAlreadyExistsException;
+import com.priscilasanfer.concrete.exception.NoSuchElementFoundException;
+import com.priscilasanfer.concrete.mapper.UserMapper;
+import com.priscilasanfer.concrete.model.User;
+import com.priscilasanfer.concrete.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private UserRepository repository;
+
+    public List<UserResponse> findAll() {
+        return repository.findAll().stream()
+                .map(UserMapper.INSTANCE::userToUserResponse)
+                .collect(Collectors.toList());
+    }
+
+    public UserResponse findById(Long id) {
+        User user = repository.findById(id)
+                .orElseThrow(() -> new NoSuchElementFoundException(id));
+        return UserMapper.INSTANCE.userToUserResponse(user);
+    }
+
+    public UserResponse create(UserRequest userRequest) {
+
+        if (emailAlreadyInUse(userRequest.getEmail())) {
+            throw new EmailAlreadyExistsException(userRequest.getEmail());
+        }
+
+        User user = UserMapper.INSTANCE.userRequestToUser(userRequest);
+        user.setCreated(Instant.now());
+        user.setModified(Instant.now());
+        user.setLastLogin(Instant.now());
+        user = repository.save(user);
+        return UserMapper.INSTANCE.userToUserResponse(user);
+    }
+
+    private boolean emailAlreadyInUse(String email) {
+        return repository.existsUserByEmail(email);
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+
+        } catch (EmptyResultDataAccessException e) {
+            throw new NoSuchElementFoundException(id);
+        }
+    }
+
+    public UserResponse update(Long id, UserUpdateRequest userUpdateRequest) {
+        User user = repository.findById(id).orElseThrow(() -> new NoSuchElementFoundException(id));
+        user.setName(userUpdateRequest.getName());
+        user.setEmail(userUpdateRequest.getEmail());
+        user.setModified(Instant.now());
+        user = repository.save(user);
+        return UserMapper.INSTANCE.userToUserResponse(user);
+    }
+}
