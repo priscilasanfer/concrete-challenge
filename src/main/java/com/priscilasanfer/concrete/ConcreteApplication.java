e69f6ecc6e3d09531794590a1db13661f3cdbbbc
Priscila Ferreira <priscilasanfer@gmail.com>
2021-02-09 08:43:08 -0300

    add initial project structure

---
+++ src/main/java/com/priscilasanfer/concrete/ConcreteApplication.java
@@ -0,0 +1,13 @@
+package com.priscilasanfer.concrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcreteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcreteApplication.class, args);
+    }
+
+}
